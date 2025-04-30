dc18d9fca3d0a6bd1b1d1b6713be35953357cc87
mahesh724077 <maheshsaini724077@gmail.com>
2025-04-30 12:32:02 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -38,7 +38,7 @@ export default function HomePage() {
     };
 
     try {
-      const res = await fetch('http://localhost:3000/api/products/register', {
+      const res = await fetch('/api/products/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
